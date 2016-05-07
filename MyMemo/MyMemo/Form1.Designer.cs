20335f40c6d2fa9b8a63ddceb6e849b07a369d70
Satoshi Terasaki <Satoshi Terasaki>
2016-05-07 13:03:03 +0900

    add My Memo

---
+++ MyMemo/MyMemo/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace MyMemo
+{
+    partial class MyMemo
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.MenuItemFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItemFIleNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItemFileOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItemFileSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItemFileSaveAs = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuFIleSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.MenuItemFileExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBoxMain = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.MenuItemSetting = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuItemFont = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuItemFile,
+            this.MenuItemSetting});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(572, 33);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // MenuItemFile
+            // 
+            this.MenuItemFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuItemFIleNew,
+            this.MenuItemFileOpen,
+            this.MenuItemFileSave,
+            this.MenuItemFileSaveAs,
+            this.MenuFIleSeparator,
+            this.MenuItemFileExit});
+            this.MenuItemFile.Name = "MenuItemFile";
+            this.MenuItemFile.Size = new System.Drawing.Size(69, 29);
+            this.MenuItemFile.Text = "File(&F)";
+            // 
+            // MenuItemFIleNew
+            // 
+            this.MenuItemFIleNew.Name = "MenuItemFIleNew";
+            this.MenuItemFIleNew.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.MenuItemFIleNew.Size = new System.Drawing.Size(230, 30);
+            this.MenuItemFIleNew.Text = "New(&N)";
+            this.MenuItemFIleNew.Click += new System.EventHandler(this.MenuItemFIleNew_Click);
+            // 
+            // MenuItemFileOpen
+            // 
+            this.MenuItemFileOpen.Name = "MenuItemFileOpen";
+            this.MenuItemFileOpen.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.MenuItemFileOpen.Size = new System.Drawing.Size(230, 30);
+            this.MenuItemFileOpen.Text = "Open(&O)";
+            this.MenuItemFileOpen.Click += new System.EventHandler(this.MenuItemFileOpen_Click);
+            // 
+            // MenuItemFileSave
+            // 
+            this.MenuItemFileSave.Name = "MenuItemFileSave";
+            this.MenuItemFileSave.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.MenuItemFileSave.Size = new System.Drawing.Size(230, 30);
+            this.MenuItemFileSave.Text = "Save(&S)";
+            this.MenuItemFileSave.Click += new System.EventHandler(this.MenuItemFileSave_Click);
+            // 
+            // MenuItemFileSaveAs
+            // 
+            this.MenuItemFileSaveAs.Name = "MenuItemFileSaveAs";
+            this.MenuItemFileSaveAs.Size = new System.Drawing.Size(230, 30);
+            this.MenuItemFileSaveAs.Text = "SaveAs(&A)";
+            this.MenuItemFileSaveAs.Click += new System.EventHandler(this.MenuItemFileSaveAs_Click);
+            // 
+            // MenuFIleSeparator
+            // 
+            this.MenuFIleSeparator.Name = "MenuFIleSeparator";
+            this.MenuFIleSeparator.Size = new System.Drawing.Size(227, 6);
+            // 
+            // MenuItemFileExit
+            // 
+            this.MenuItemFileExit.Name = "MenuItemFileExit";
+            this.MenuItemFileExit.Size = new System.Drawing.Size(230, 30);
+            this.MenuItemFileExit.Text = "Exit(&X)";
+            this.MenuItemFileExit.Click += new System.EventHandler(this.MenuItemFileExit_Click);
+            // 
+            // textBoxMain
+            // 
+            this.textBoxMain.Font = new System.Drawing.Font("MS UI Gothic", 12F);
+            this.textBoxMain.Location = new System.Drawing.Point(12, 36);
+            this.textBoxMain.Name = "textBoxMain";
+            this.textBoxMain.Size = new System.Drawing.Size(100, 31);
+            this.textBoxMain.TabIndex = 2;
+            this.textBoxMain.TextChanged += new System.EventHandler(this.textBoxMain_TextChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // MenuItemSetting
+            // 
+            this.MenuItemSetting.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuItemFont});
+            this.MenuItemSetting.Name = "MenuItemSetting";
+            this.MenuItemSetting.Size = new System.Drawing.Size(100, 29);
+            this.MenuItemSetting.Text = "Setting(&S)";
+            // 
+            // MenuItemFont
+            // 
+            this.MenuItemFont.Name = "MenuItemFont";
+            this.MenuItemFont.Size = new System.Drawing.Size(211, 30);
+            this.MenuItemFont.Text = "Font";
+            this.MenuItemFont.Click += new System.EventHandler(this.MenuItemFont_Click);
+            // 
+            // MyMemo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(572, 471);
+            this.Controls.Add(this.textBoxMain);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MyMemo";
+            this.Text = "MyMemo";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MyMemo_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MyMemo_FormClosed);
+            this.Load += new System.EventHandler(this.MyMemo_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemFile;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemFileExit;
+        private System.Windows.Forms.TextBox textBoxMain;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemFIleNew;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemFileOpen;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemFileSave;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemFileSaveAs;
+        private System.Windows.Forms.ToolStripSeparator MenuFIleSeparator;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemSetting;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemFont;
+        private System.Windows.Forms.FontDialog fontDialog1;
+    }
+}
+
---
+++ MyMemo/MyMemo/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyMemo
+{
+    public partial class MyMemo : Form
+    {
+        const string ApplicationName = "MyMemo";
+        const string emptyFileName = "";
+        const string RegistryKey = @"TerasakiSoftware" + ApplicationName;
+        private string currentFilePath;
+        /*プロパティの五十川にあって値を保持する変数．
+        他の部分からは必ずプロパティを介して読み書きすると決める．
+        */
+        private string currentFileNameValue;
+        private string currentFileName
+        {
+            get { return currentFileNameValue; }
+            set
+            {
+                currentFileNameValue = value;
+                if(value != emptyFileName)
+                {
+                    currentFilePath = System.IO.Path.GetDirectoryName(value);
+                }
+                Edited = false;
+            }
+        }
+
+        private bool EditedValue;
+        private bool Edited
+        {
+            get { return EditedValue; }
+            set
+            {
+                EditedValue = value;
+                UpdateStatus();
+            }
+        }
+
+        private void UpdateStatus()
+        {
+            string s = ApplicationName;
+            if (currentFileName != emptyFileName)
+            {
+                s += " - " + System.IO.Path.GetFileName(currentFileName);
+            }
+            if (Edited)
+            {   
+                s += " - " + System.IO.Path.GetFileName(currentFileName) + "*";
+                MenuItemFileSave.Enabled = true;
+                MenuItemFileSaveAs.Enabled = true;
+            }
+            else
+            {
+                MenuItemFileSave.Enabled = false;
+                MenuItemFileSaveAs.Enabled = false;
+            }
+            //Form の名前を読み込んだファイルとする．
+            this.Text = s;
+        }
+        public MyMemo()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItemFileExit_Click(object sender, EventArgs e)
+        {
+
+            Application.Exit();
+        }
+
+        private void MyMemo_Load(object sender, EventArgs e)
+        {
+            // 起動時の初期設定のコードをここに記述してください．
+            currentFileName = emptyFileName;
+            textBoxMain.Multiline = true;
+            textBoxMain.ScrollBars = ScrollBars.Vertical;
+            textBoxMain.Dock = DockStyle.Fill;
+            saveFileDialog1.Filter = "テキスト文書|*.txt|すべてのファイル|*.*";
+
+            Microsoft.Win32.RegistryKey regKey = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(RegistryKey);
+            currentFilePath = regKey.GetValue("currentFilePath", System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)).ToString();
+
+            fontDialog1.ShowEffects = false;
+            fontDialog1.AllowScriptChange = false;
+
+            if (1 < Environment.GetCommandLineArgs().Length)
+            {
+                string[] args = Environment.GetCommandLineArgs();
+                LoadFile(args[1]);
+            }
+        }
+
+        private void MenuItemFileOpen_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "";
+            if (!AskGiveUpText())
+            {
+                return;
+            }
+            else
+            {
+                openFileDialog1.InitialDirectory = currentFilePath;
+                openFileDialog1.FileName = "";
+                if (DialogResult.OK == openFileDialog1.ShowDialog())
+                {
+                    LoadFile(openFileDialog1.FileName);
+                }
+            }
+        }
+        /**
+        value で指定したPathの存在を確認しファイルをロードする．
+        */
+        private void LoadFile(string value)
+        {
+            if (System.IO.File.Exists(value))
+            {
+
+            textBoxMain.Text = System.IO.File.ReadAllText(
+                value, System.Text.Encoding.GetEncoding("Shift_JIS"));
+            currentFileName = value;
+            //text の全選択を防ぐための対応
+            textBoxMain.Select(0, 0);
+            }
+            else
+            {
+                MessageBox.Show(value + " not found",ApplicationName);
+            }
+        }
+
+        private void MenuItemFileSaveAs_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.InitialDirectory = currentFilePath;
+            saveFileDialog1.FileName = System.IO.Path.GetFileName(currentFileName);
+            if (DialogResult.OK == saveFileDialog1.ShowDialog())
+            {
+                SaveFile(saveFileDialog1.FileName);
+            }
+        }
+
+        private void SaveFile(string value)
+        {
+            System.IO.File.WriteAllText(value, textBoxMain.Text, System.Text.Encoding.GetEncoding("Shift_JIS"));
+            currentFileName = value;
+        }
+
+        private void MenuItemFileSave_Click(object sender, EventArgs e)
+        {
+            if (System.IO.Path.GetFileName(currentFileName)==emptyFileName)
+            {
+                MenuItemFileSaveAs_Click(sender, e);
+            }
+            else
+            {
+                SaveFile(currentFileName);
+            }
+        }
+
+        private void textBoxMain_TextChanged(object sender, EventArgs e)
+        {
+            Edited = true;
+        }
+
+        private void MyMemo_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!AskGiveUpText())
+            {
+                e.Cancel = true;
+            }
+        }
+
+
+        private bool AskGiveUpText()
+        {
+            if (!Edited || textBoxMain.TextLength == 0)
+            {
+                return true;
+            }
+            if (DialogResult.Yes == MessageBox.Show("編集内容を破棄しますか？？",ApplicationName,MessageBoxButtons.YesNo,MessageBoxIcon.Warning))
+            {
+                return true;
+            }            
+            else
+            {
+                return false;
+            }
+        }
+
+        private void MenuItemFIleNew_Click(object sender, EventArgs e)
+        {
+            if (!AskGiveUpText())
+            {
+                return;
+            }
+            textBoxMain.Clear();
+            currentFileName = emptyFileName;
+        }
+
+        private void MyMemo_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Microsoft.Win32.RegistryKey regKey = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(RegistryKey);
+            regKey.SetValue("currentFilePath", currentFilePath);
+        }
+
+        private void MenuItemFont_Click(object sender, EventArgs e)
+        {
+            fontDialog1.Font = textBoxMain.Font;
+            if (DialogResult.OK == fontDialog1.ShowDialog())
+            {
+                textBoxMain.Font = fontDialog1.Font;
+            }
+
+        }
+    }
+}
